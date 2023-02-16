ca201278fbe26d6c7d5485443b6f2d4a32b97745
Tedanil <anildayi@hotmail.com>
2023-02-16 19:01:29 +0300

    login component edited

--- ETicaretClient/src/app/ui/components/login/login.component.ts
+++ ETicaretClient/src/app/ui/components/login/login.component.ts
@@ -4,7 +4,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { AuthService } from 'src/app/services/common/auth.service';
 import { UserService } from 'src/app/services/common/models/user.service';
-import { FacebookLoginProvider, GoogleLoginProvider, GoogleSigninButtonDirective, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { async } from '@angular/core/testing';
 import { UserAuthService } from 'src/app/services/common/models/user-auth.service';
 import { FormGroup } from '@angular/forms';
